ccfb1cbfc4ab742efe47a615d3365d3f1c3eb298
Jeremy <dmsjerk@gmail.com>
2017-06-03 17:21:03 -0500

    Added homedirectory variable

--- first_script.sh
+++ first_script.sh
@@ -3,7 +3,8 @@ sudo apt-get update
 sudo apt-get install bzip2
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
 bash Miniconda2-latest-Linux-x86_64.sh
-export PATH=$PATH:/home/dmsjerk/miniconda2/bin
+homedir=`eval echo ~$USER`
+export PATH=$PATH:$homedir/miniconda2/bin
 conda install scikit-learn
 conda install pandas
 conda install jupyter
