5cc440b7bd0a214214984a8a5a96d02e98a3193a
Jeremy <dmsjerk@gmail.com>
2017-06-03 17:07:18 -0500

    Tested until through Jupyter

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,9 @@
+cd
 sudo apt-get update
 sudo apt-get install bzip2
 wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda2-latest-Linux-x86_64.sh
+export PATH=$PATH:/home/dmsjerk/miniconda2/bin
+conda install scikit-learn
+conda install pandas
+conda install jupyter
